0d1133b2fb8317e3f636b22dc11eeb19465363f6
Laurant Marijnissen <laurant@gigafyde.dev>
2021-03-25 15:26:57 +0100

    Volume rewrite

--- src/main/java/dev/gigafyde/apollo/commands/music/Volume.java
+++ src/main/java/dev/gigafyde/apollo/commands/music/Volume.java
@@ -2,10 +2,9 @@ package dev.gigafyde.apollo.commands.music;
 
 /*
  Created by GigaFyde
-  https://github.com/GigaFyde
+ https://github.com/GigaFyde
  */
 
-import dev.gigafyde.apollo.core.MusicManager;
 import dev.gigafyde.apollo.core.command.Command;
 import dev.gigafyde.apollo.core.command.CommandEvent;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -19,15 +18,14 @@ public class Volume extends Command {
 
     public void execute(CommandEvent event) {
         TextChannel channel = event.getTextChannel();
-        MusicManager musicManager = event.getClient().getMusicManager();
         if (event.getArgument().isEmpty()) {
-            int vol = musicManager.getScheduler(event.getGuild()).getPlayer().getVolume();
+            int vol = (int) (event.getClient().getLavalink().getLink(event.getGuild()).getPlayer().getFilters().getVolume() * 100);
             channel.sendMessage("\uD83D\uDD0A **Current volume is: " + vol + "%**").queue();
         } else {
             try {
-                int volume = Integer.parseInt(event.getArgument());
-                musicManager.getScheduler(event.getGuild()).getPlayer().setVolume(volume);
-                channel.sendMessage("\uD83D\uDD0A **Volume set to: " + musicManager.getScheduler(event.getGuild()).getPlayer().getVolume() + "%**").queue();
+                float volume = (float) (Integer.parseInt(event.getArgument()) * 0.01);
+                event.getClient().getLavalink().getLink(event.getGuild()).getPlayer().getFilters().setVolume(volume).commit();
+                channel.sendMessage("\uD83D\uDD0A **Volume set to: " + (event.getClient().getLavalink().getLink(event.getGuild()).getPlayer().getFilters().getVolume() * 100) + "%**").queue();
             } catch (NumberFormatException ignored) {
                 channel.sendMessage("\u274C **Invalid number**").queue();
             }
