c4552a78bc663d6ec6848aa67c2d5688d8862f67
Laurant Marijnissen <laurant@gigafyde.dev>
2021-08-29 18:08:10 +0200

    Remove deprecated code

--- src/main/java/dev/gigafyde/apollo/utils/SongUtils.java
+++ src/main/java/dev/gigafyde/apollo/utils/SongUtils.java
@@ -36,18 +36,4 @@ public class SongUtils {
         String duration = calculateSongLength(track);
         return String.format("%d:%02d/%s", pmin, psec, duration);
     }
-
-    static String generateProgressBar(AudioTrack track) {
-        int size = 15;
-        long position = track.getPosition();
-        long duration = track.getDuration();
-        double percent = (size + 0.0) * position / duration;
-        percent = Math.floor(percent);
-        StringBuilder s = new StringBuilder();
-        for (int i = 0; i < size - 1; i++) {
-            if (percent > i * (size / 15)) s.append("[O]");
-            else s.append("[_]");
-        }
-        return s.toString();
-    }
 }
