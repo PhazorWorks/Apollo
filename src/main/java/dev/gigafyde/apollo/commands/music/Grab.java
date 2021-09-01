d555464bb12c4f2866a97e4af2e8561d61237144
Laurant Marijnissen <laurant@gigafyde.dev>
2021-09-01 17:29:43 +0200

    Check for playing track before attempting to grab it

--- src/main/java/dev/gigafyde/apollo/commands/music/Grab.java
+++ src/main/java/dev/gigafyde/apollo/commands/music/Grab.java
@@ -18,13 +18,17 @@ public class Grab extends Command {
     public void execute(CommandEvent event) {
         TextChannel channel = event.getTextChannel();
         TrackScheduler scheduler = event.getClient().getMusicManager().getScheduler(event.getGuild());
-        String uri = scheduler.getPlayer().getPlayingTrack().getInfo().uri;
-        User author = event.getAuthor();
-        try {
-            author.openPrivateChannel().complete().sendMessage("Here is a copy of the currently playing track\n" + uri).complete();
-            event.getTrigger().addReaction(Emoji.SUCCESS.toString()).queue();
-        } catch (Exception e) {
-            channel.sendMessage(author.getAsMention() + " Hi there, I tried to send the link to you privately, but it seems that failed, so I'm sending it here instead.\n" + uri).queue();
+        if (scheduler.getPlayer().getPlayingTrack() != null) {
+            String uri = scheduler.getPlayer().getPlayingTrack().getInfo().uri;
+            User author = event.getAuthor();
+            try {
+                author.openPrivateChannel().complete().sendMessage("Here is a copy of the currently playing track\n" + uri).complete();
+                event.getTrigger().addReaction(Emoji.SUCCESS.toString()).queue();
+            } catch (Exception e) {
+                channel.sendMessage(author.getAsMention() + " Hi there, I tried to send the link to you privately, but it seems that failed, so I'm sending it here instead.\n" + uri).queue();
+            }
+        } else {
+            event.getTrigger().reply("Nothing is currently playing, so there was nothing to grab.").mentionRepliedUser(true).queue();
         }
     }
 }
