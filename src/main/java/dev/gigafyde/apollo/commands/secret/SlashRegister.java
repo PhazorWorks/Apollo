7f5ce8782c3b1a42aea7b0a128635b06c512727b
Laurant Marijnissen <laurant@gigafyde.dev>
2021-10-01 20:43:23 +0200

    Convert slashregister into a command instead

--- src/main/java/dev/gigafyde/apollo/commands/secret/SlashRegister.java
+++ src/main/java/dev/gigafyde/apollo/commands/secret/SlashRegister.java
@@ -1,4 +1,4 @@
-package dev.gigafyde.apollo.core.handlers;
+package dev.gigafyde.apollo.commands.secret;
 
 /*
  Created by GigaFyde
@@ -6,19 +6,22 @@ package dev.gigafyde.apollo.core.handlers;
  */
 
 
-import dev.gigafyde.apollo.Main;
+import dev.gigafyde.apollo.core.command.Command;
+import dev.gigafyde.apollo.core.command.CommandEvent;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.CommandType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import org.jetbrains.annotations.NotNull;
 
-public class SlashRegister extends ListenerAdapter {
+public class SlashRegister extends Command {
+    public SlashRegister() {
+        this.name = "slash";
+        this.description = "";
+        this.triggers = new String[]{"slash"};
+        this.ownerOnly = true;
+    }
 
-    @Override
-    public void onReady(@NotNull ReadyEvent event) {
+    public void execute(CommandEvent event) {
         JDA jda = event.getJDA();
         // Slash commands
         CommandData ping = new CommandData("ping", "pong");
@@ -29,6 +32,7 @@ public class SlashRegister extends ListenerAdapter {
         CommandData loop = new CommandData("loop", "Loop track or queue");
         CommandData nowPlaying = new CommandData("now-playing", "Shows current playing song");
         CommandData pause = new CommandData("pause", "Pauses the player");
+        CommandData volume = new CommandData("volume", "Gets or sets a new volume").addOption(OptionType.STRING, "input", "Sets the new volume", false);
         // Message context commands
         CommandData addToQueue = new CommandData(CommandType.MESSAGE_CONTEXT, "Add to Queue");
         // Register slash commands
@@ -41,16 +45,14 @@ public class SlashRegister extends ListenerAdapter {
 //        jda.upsertCommand(pause).queue();
         // Register message context commands
 //           jda.upsertCommand(addToQueue).queue();
-        if (!(Main.LYRICS_WEB_SERVER == null) & !(Main.LYRICS_API_KEY == null)) {
-            // Only register if those values are defined
-            jda.upsertCommand(lyrics).queue();
-        } else {
-            try {
-                jda.deleteCommandById("lyrics").queue();
-            } catch (Exception ignored) {
-                // As per the JDA docs, this will only fail with ErrorResponse.UNKNOWN_COMMAND if there's no such command registered.
-                // Which will mean it'll be already unregistered.
-            }
-        }
+//        if (!(Main.LYRICS_WEB_SERVER == null) & !(Main.LYRICS_API_KEY == null)) {
+        // Only register if those values are defined
+//            jda.upsertCommand(lyrics).queue();
+//        } else {
+//            try {
+//                jda.deleteCommandById("lyrics").queue();
+//            } catch (Exception ignored) {
+        // As per the JDA docs, this will only fail with ErrorResponse.UNKNOWN_COMMAND if there's no such command registered.
+        // Which will mean it'll be already unregistered.
     }
 }
