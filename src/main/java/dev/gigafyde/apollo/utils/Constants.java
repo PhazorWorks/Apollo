ae823a7903cc7d48ab5f0b710cc6c84c0c26a88a
SyntaxDragon <dragon@dragondev.party>
2021-11-21 21:31:41 +0000

    refactor: fix error messages

--- src/main/java/dev/gigafyde/apollo/utils/Constants.java
+++ src/main/java/dev/gigafyde/apollo/utils/Constants.java
@@ -1,8 +1,8 @@
 package dev.gigafyde.apollo.utils;
 
 public class Constants {
-    public static String requireActivePlayerCommand = "**This command can only be used when there is a track playing!**";
-    public static String botNotInVC = "**This command can only be used if I am in a voice channel!";
+    public static String requireActivePlayerCommand = "**This command can only be used when there is a active track playing!**";
+    public static String botNotInVC = "**This command can only be used if I am in a voice channel!**";
     public static String userNotInVC = "**Please join my voice channel if you wish to use this command!**";
     public static String usedElsewhere = "**I am already connected in a different voice channel!**";
     public static String unableToJoinVC = "**I am unable to join this voice channel!**";
