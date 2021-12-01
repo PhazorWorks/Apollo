b2e5c75a3230e2484d70a80b9e8b173bde98b359
SyntaxDragon <dragon@dragondev.party>
2021-12-01 20:03:38 +0000

    feat: add error handling to spotify handler

--- src/main/java/dev/gigafyde/apollo/core/handlers/SpotifyHandler.java
+++ src/main/java/dev/gigafyde/apollo/core/handlers/SpotifyHandler.java
@@ -8,13 +8,14 @@ package dev.gigafyde.apollo.core.handlers;
 
 import dev.gigafyde.apollo.Main;
 import dev.gigafyde.apollo.core.TrackScheduler;
-import java.util.Objects;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class SpotifyHandler {
     private static final Logger log = LoggerFactory.getLogger("SpotifyHandler");
@@ -41,13 +42,16 @@ public class SpotifyHandler {
                     new Request.Builder()
                             .url(Main.SPOTIFY_WEB_SERVER + "track" + "?id=" + trackId)
                             .build()).execute();
+            if (!response.isSuccessful()) {
+                throw new Exception("Track with id " + trackId + " was not found");
+            }
             JSONObject jsonResponse = new JSONObject(Objects.requireNonNull(response.body()).string());
             JSONObject jsonObject = jsonResponse.getJSONObject("track");
             JSONObject artistObject = jsonObject.getJSONArray("artists").getJSONObject(0);
             String artist = artistObject.get("name").toString();
             String title = jsonObject.get("name").toString();
             String explicit = Boolean.parseBoolean(jsonObject.get("explicit").toString()) ? "explicit" : "";
-            SongHandler.loadHandler(scheduler, artist + " " + title +  " " + explicit, true, true);
+            SongHandler.loadHandler(scheduler, artist + " " + title + " " + explicit, true, true);
         } catch (Exception e) {
             SongCallBackListener.notifySpotifyAbort(e);
             log.error("Spotify Lookup failed! Aborting");
@@ -60,6 +64,9 @@ public class SpotifyHandler {
                     new Request.Builder()
                             .url(Main.SPOTIFY_WEB_SERVER + "playlist" + "?id=" + playlistId)
                             .build()).execute();
+            if (!response.isSuccessful()) {
+                throw new Exception("Playlist with id " + playlistId + " was not found");
+            }
             JSONObject jsonResponse = new JSONObject(Objects.requireNonNull(response.body()).string());
             JSONObject jsonObject = jsonResponse.getJSONObject("playlist");
             JSONArray tracks = jsonObject.getJSONArray("tracks");
