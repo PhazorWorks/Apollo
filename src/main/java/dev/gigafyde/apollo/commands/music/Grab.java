18706a74fa28f0191ee1748a28680dd22be33aae
SyntaxDragon <dragon@dragondev.party>
2023-04-12 14:42:36 +0100

    refactor: merge grab and link into one and volume alias

--- src/main/java/dev/gigafyde/apollo/commands/music/Grab.java
+++ src/main/java/dev/gigafyde/apollo/commands/music/Grab.java
@@ -1,10 +1,16 @@
 package dev.gigafyde.apollo.commands.music;
 
+import dev.gigafyde.apollo.Main;
 import dev.gigafyde.apollo.core.TrackScheduler;
 import dev.gigafyde.apollo.core.command.Command;
 import dev.gigafyde.apollo.core.command.CommandEvent;
 import dev.gigafyde.apollo.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import dev.gigafyde.apollo.utils.SongUtils;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.emoji.*;
 
@@ -13,8 +19,8 @@ public class Grab extends Command {
 
     public Grab() {
         this.name = "grab";
-        this.description = "sends the currently playing song to your dm";
-        this.triggers = new String[]{"grab", "save"};
+        this.description = "sends the current song";
+        this.triggers = new String[]{"grab", "save", "link"};
         this.guildOnly = true;
     }
 
@@ -35,22 +41,30 @@ public class Grab extends Command {
             event.sendError(Constants.requireActivePlayerCommand);
             return;
         }
+
         if (scheduler.getPlayer().getPlayingTrack() != null) {
             String uri = scheduler.getPlayer().getPlayingTrack().getInfo().uri;
             User author = event.getAuthor();
-            try {
+            if (event.getArgument().isEmpty()) {
                 switch (event.getCommandType()) {
-                    case MESSAGE -> {
-                        author.openPrivateChannel().complete().sendMessage("Here is a copy of the currently playing track\n" + uri).complete();
-                        event.getMessage().addReaction(Emoji.fromUnicode(String.valueOf(dev.gigafyde.apollo.utils.Emoji.SUCCESS))).queue();
-                    }
-                    case SLASH -> event.send("Here is a copy of the currently playing track\n" + uri);
+                    case MESSAGE ->
+                            event.getMessage().reply("Currently playing: " + uri).queue();
+                    case SLASH ->
+                            event.getHook().editOriginal("Currently playing: " + uri).queue();
                 }
-            } catch (Exception e) {
-                event.sendError("Hi there, I tried to send the link to you privately, but it seems that failed, so I'm sending it here instead.\n" + uri + "\n" + e);
+            } else {
+                try {
+                    switch (event.getCommandType()) {
+                        case MESSAGE -> {
+                            author.openPrivateChannel().complete().sendMessage("Here is a copy of the currently playing track\n" + uri).complete();
+                            event.getMessage().addReaction(Emoji.fromUnicode(String.valueOf(dev.gigafyde.apollo.utils.Emoji.SUCCESS))).queue();
+                        }
+                        case SLASH -> event.send("Here is a copy of the currently playing track\n" + uri);
+                    }
+                } catch (Exception e) {
+                    event.sendError("Hi there, I tried to send the link to you privately, but it seems that failed, so I'm sending it here instead.\n" + uri + "\n" + e);
+                }
             }
-        } else {
-            event.sendError(Constants.requireActivePlayerCommand);
         }
     }
 }
