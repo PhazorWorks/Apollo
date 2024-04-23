68faab28647d0621a6332eeb883e2d7c6c281d7e
Laurant Marijnissen <laurant@gigafyde.dev>
2024-04-23 10:33:35 +0200

    implement new youtube source manager for lavaplayer

--- src/main/java/dev/gigafyde/apollo/core/MusicManager.java
+++ src/main/java/dev/gigafyde/apollo/core/MusicManager.java
@@ -5,14 +5,28 @@ package dev.gigafyde.apollo.core;
  https://github.com/GigaFyde
  */
 
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerRegistry;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.beam.BeamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.getyarn.GetyarnAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.nico.NicoAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.yamusic.YandexMusicAudioSourceManager;
+import dev.lavalink.youtube.clients.AndroidWithThumbnail;
+import dev.lavalink.youtube.clients.MusicWithThumbnail;
+import dev.lavalink.youtube.clients.WebWithThumbnail;
+import dev.lavalink.youtube.clients.skeleton.Client;
 import net.dv8tion.jda.api.entities.Guild;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
 
 public final class MusicManager {
     private final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
@@ -22,8 +36,17 @@ public final class MusicManager {
     public MusicManager() {
 
         this.musicManagers = new HashMap<>();
-        AudioSourceManagers.registerRemoteSources(playerManager);
-        AudioSourceManagers.registerLocalSource(playerManager);
+        playerManager.registerSourceManager(new YoutubeAudioSourceManager(/*allowSearch:*/ true, new Client[]{new MusicWithThumbnail(), new WebWithThumbnail(), new AndroidWithThumbnail()}));
+        playerManager.registerSourceManager(new YandexMusicAudioSourceManager(true));
+        playerManager.registerSourceManager(SoundCloudAudioSourceManager.createDefault());
+        playerManager.registerSourceManager(new BandcampAudioSourceManager());
+        playerManager.registerSourceManager(new VimeoAudioSourceManager());
+        playerManager.registerSourceManager(new TwitchStreamAudioSourceManager());
+        playerManager.registerSourceManager(new BeamAudioSourceManager());
+        playerManager.registerSourceManager(new GetyarnAudioSourceManager());
+        playerManager.registerSourceManager(new NicoAudioSourceManager());
+//        playerManager.registerSourceManager(new HttpAudioSourceManager(MediaContainerRegistry.DEFAULT_REGISTRY));
+
     }
 
     public static synchronized MusicManager getInstance() {
